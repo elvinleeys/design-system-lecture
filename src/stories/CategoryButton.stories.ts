83b5b48bdc76aed04d0294db4805b86ca6e44e9e
Elvin-lee <herjun802@gmail.com>
2025-04-08 21:03:02 +0900

    Create a category button story

---
+++ src/stories/CategoryButton.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from "@storybook/test";
+import CategoryButton from '../components/CategoryButton';
+
+const meta = {
+  title: 'Buttons/CategoryButton',
+  component: CategoryButton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    iconAlt: { 
+      control: "text", 
+      description: "이미지의 alt 속성", 
+      defaultValue: "icon", 
+    },
+    iconPath: { 
+      control: "text", 
+      description: "이미지의 경로", 
+      defaultValue: "",
+    },
+    text: {
+      control: "text",
+      description: "버튼에 표시될 텍스트",
+      defaultValue: "카테고리",
+    },
+    onClick: { action: "clicked", description: "버튼 클릭 이벤트"},
+  },
+  args: {
+    onClick: fn(),
+  }
+} satisfies Meta<typeof CategoryButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    iconAlt: "icon",
+    text: "카테고리",
+    iconPath: "https://kr.object.ncloudstorage.com/icons/ic-category.svg",
+  },
+};
