1bb249a914dd198f4078a58910d0870e1d047e32
Elvin-lee <herjun802@gmail.com>
2025-04-09 21:31:31 +0900

    Add accessibility test workflow

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -11,6 +11,37 @@ const preview: Preview = {
        date: /Date$/i,
       },
     },
+
+    a11y: {
+      // Optional selector to inspect
+      element: 'body',
+      config: {
+        rules: [
+          {
+            // The autocomplete rule will not run based on the CSS selector provided
+            id: 'autocomplete-valid',
+            selector: '*:not([autocomplete="nope"])',
+          },
+          {
+            // Setting the enabled option to false will disable checks for this particular rule on all stories.
+            id: 'image-alt',
+            enabled: false,
+          },
+        ],
+      },
+      /*
+       * Axe's options parameter
+       * See https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter
+       * to learn more about the available options.
+       */
+      options: {},
+    },
+    globals: {
+      a11y: {
+        // Optional flag to prevent the automatic check
+        manual: true,
+      },
+    },
   },
 };
 
